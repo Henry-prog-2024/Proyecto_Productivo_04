749f3e19bf5a56579b78edcea078da93bb1ba75d
hferruzo <i2320225@continental.edu.pe>
2025-11-07 14:05:28 -0500

    mostar barra

--- app.py
+++ app.py
@@ -356,8 +356,8 @@ if st.sidebar.button("🎯 Predecir Probabilidad de Compra", type="primary"):
                 st.success(f"✅ Predicción completada para el cliente DNI **{dni_cliente}**")
                 #st.metric("Probabilidad de Compra", f"{probabilidad*100:.2f}%")
                 # Mostrar probabilidad con barra de progreso
-                col1, col2 = st.columns([1, 2])
-                st.progress(float(probabilidad))
+                #col1, col2 = st.columns([1, 2])
+                #st.progress(float(probabilidad))
 
                 # Crear registro completo con todos los datos + resultado
                 registro_completo = input_data.copy()
